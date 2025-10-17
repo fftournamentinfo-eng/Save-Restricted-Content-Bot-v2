17863684c44e6b0b0c1a1394d42049ccf7149fe9
fftournamentinfo-eng <fftournament.info@gmail.com>
2025-10-17 22:59:20 +0530

    Update __main__.py

--- devgagan/__main__.py
+++ devgagan/__main__.py
@@ -1,17 +1,3 @@
-# ---------------------------------------------------
-# File Name: __main__.py
-# Description: A Pyrogram bot for downloading files from Telegram channels or groups 
-#              and uploading them back to Telegram.
-# Author: Gagan
-# GitHub: https://github.com/devgaganin/
-# Telegram: https://t.me/team_spy_pro
-# YouTube: https://youtube.com/@dev_gagan
-# Created: 2025-01-11
-# Last Modified: 2025-01-11
-# Version: 2.0.5
-# License: MIT License
-# ---------------------------------------------------
-
 import asyncio
 import importlib
 import gc
@@ -40,10 +26,10 @@ async def devggn_boot():
 📂 Bot Deployed successfully ...
 📝 Description: A Pyrogram bot for downloading files from Telegram channels or groups 
                 and uploading them back to Telegram.
-👨‍💻 Author: Saurabh
-🌐 GitHub: https://github.com/devgaganin/
+👨‍💻 Author: saurabh
+🌐 GitHub:
 📬 Telegram: https://t.me/saurabh_patel9412
-▶️ YouTube: https://youtube.com/@dev_gagan
+▶️ YouTube:
 🗓️ Created: 2025-01-11
 🔄 Last Modified: 2025-01-11
 🛠️ Version: 2.0.5
